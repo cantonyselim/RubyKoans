11ce2e5488eaea0b6ba056bbdeb9af80011da662
Christopher Selim <cantonyselim@gmail.com>
2015-06-23 15:12:51 -0700

    next test== creating hashes

--- about_objects.rb
+++ about_objects.rb
@@ -10,18 +10,18 @@ class AboutObjects < Neo::Koan
   end
 
   def test_objects_can_be_converted_to_strings
-    assert_equal true, 123.to_s
-    assert_equal true, nil.to_s
+    assert_equal "123", 123.to_s
+    assert_equal "", nil.to_s
   end
 
   def test_objects_can_be_inspected
-    assert_equal true, 123.inspect
-    assert_equal true, nil.inspect
+    assert_equal "123", 123.inspect
+    assert_equal "nil", nil.inspect
   end
 
   def test_every_object_has_an_id
     obj = Object.new
-    assert_equal true, obj.object_id.class
+    assert_equal Fixnum, obj.object_id.class
   end
 
   def test_every_object_has_different_id
@@ -31,10 +31,10 @@ class AboutObjects < Neo::Koan
   end
 
   def test_small_integers_have_fixed_ids
-    assert_equal __, 0.object_id
-    assert_equal __, 1.object_id
-    assert_equal __, 2.object_id
-    assert_equal __, 100.object_id
+    assert_equal 1, 0.object_id
+    assert_equal 3, 1.object_id
+    assert_equal 5, 2.object_id
+    assert_equal 201, 100.object_id
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
@@ -44,7 +44,7 @@ class AboutObjects < Neo::Koan
     obj = Object.new
     copy = obj.clone
 
-    assert_equal __, obj           != copy
-    assert_equal __, obj.object_id != copy.object_id
+    assert_equal true, obj           != copy
+    assert_equal true, obj.object_id != copy.object_id
   end
 end
